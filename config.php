ab3a37181fb5c6bc3bfe6d373476c5f84e9399af
Jeffrey Geyssens <jeffrey@humanized.be>
2018-03-21 15:08:58 +0100

    Organisation Initialisation

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 return [
     'id' => 'organisations',
     'class' => 'humhub\modules\organisation\Module',
-    'namespace' => 'humhub\modules\organisations',
+    'namespace' => 'humhub\modules\organisation',
     'events' => [
        // ['class' => \humhub\widgets\TopMenu::className(), 'event' => \humhub\widgets\TopMenu::EVENT_INIT, 'callback' => ['humhub\modules\contact\Events', 'onTopMenuInit']],
     ],
