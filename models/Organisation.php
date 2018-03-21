f1ecc20a405c4978ec250c59bdbdb32a7d8ea79a
Jeffrey Geyssens <jeffrey@humanized.be>
2018-03-21 14:46:13 +0100

    Organisation Initialisation

--- models/Organisation.php
+++ models/Organisation.php
@@ -1,1 +1,81 @@
+<?php
 
+/**
+ * @link https://www.humanized.it/
+ * @copyright Copyright (c) 2018 Humanized
+ * @license https://www.humhub.com/licences
+ */
+
+namespace humhub\modules\organisations\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "profile".
+ *
+ * @property integer $user_id
+ * @property string $firstname
+ * @property string $lastname
+ * @property string $title
+ * @property string $gender
+ * @property string $street
+ * @property string $zip
+ * @property string $city
+ * @property string $country
+ * @property string $state
+ * @property integer $birthday_hide_year
+ * @property string $birthday
+ * @property string $about
+ * @property string $phone_private
+ * @property string $phone_work
+ * @property string $mobile
+ * @property string $fax
+ * @property string $im_skype
+ * @property string $im_msn
+ * @property integer $im_icq
+ * @property string $im_xmpp
+ * @property string $url
+ * @property string $url_facebook
+ * @property string $url_linkedin
+ * @property string $url_xing
+ * @property string $url_youtube
+ * @property string $url_vimeo
+ * @property string $url_flickr
+ * @property string $url_myspace
+ * @property string $url_googleplus
+ * @property string $url_twitter
+ */
+class Organisation extends \yii\db\ActiveRecord
+{
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'organisation';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name','mail_slug'], 'string'],
+            [['gallery_media_id'], 'integer'],
+            [['gallery_media_id'], 'integer'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        $labels = [];
+        return $labels;
+    }
+
+}
