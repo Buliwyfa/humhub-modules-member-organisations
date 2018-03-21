396ffc97007b35ad1b2eadbac65e05becaa81af1
jeffrico <jeffrey@humanized.be>
2018-03-21 13:45:13 +0100

    config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'id' => 'organisations',
+    'class' => 'humhub\modules\organisation\Module',
+    'namespace' => 'humhub\modules\organisations',
+    'events' => [
+       // ['class' => \humhub\widgets\TopMenu::className(), 'event' => \humhub\widgets\TopMenu::EVENT_INIT, 'callback' => ['humhub\modules\contact\Events', 'onTopMenuInit']],
+    ],
+];
