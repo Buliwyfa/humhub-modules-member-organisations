5edc9ced57cd30518be0f19e5ea407c7146527ba
jeffrico <jeffrey@humanized.be>
2018-03-21 13:46:33 +0100

    Create Module.php

---
+++ Module.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace humhub\modules\organisations;
+
+use Yii;
+use yii\helpers\Url;
+
+class Module extends \humhub\components\Module
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function getConfigUrl()
+    {
+        return Url::to([
+                    '/organisations/config'
+        ]);
+    }
+
+}
